2e9f31ee57fa7d90e7743ad6e26c2d5250d2f133
Jason Huels and Joel Stockamp <joel@>
2019-08-14 14:40:08 -0700

    get the calc to almost work

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,56 @@
 $(document).ready(function(){
-  var num1;
-  var num2;
-  var operator;
+  var num1 = "";
+  var num2 = "";
+  var result = ""
+  var operator = "";
 
   $("#button-land button.num").click(function() {
+    if(result !== "") {
+      result = "";
+      $("#display").text("");
+    }
     $("#display").append(this.innerHTML);
+
   });
   $("#button-land button.ops").click(function() {
-    num1 = $("#display").text();
-    
+    if(num1 === "") {
+      num1 = parseInt($("#display").text());
+      $("#display").text("");
+      operator = this.id;
+      console.log(operator);
+    } else {
+      num2 = parseInt($("#display").text());
+      $("#display").text("");
+      switch(operator) {
+        case "add":
+          result = add(num1, num2);
+          $("#display").append(result);
+          operator = "";
+          break;
+        case "sub":
+          result = sub(num1, num2);
+          $("#display").append(result);
+          operator = "";
+          break;
+        case "divide":
+          result = divide(num1, num2);
+          $("#display").append(result);
+          operator = "";
+          break;
+        case "mult":
+          result = mult(num1, num2);
+          $("#display").append(result);
+          operator = "";
+          break;
+        default:
+          num1 = "";
+          num2 = "";
+          operator = "";
+          break;
+
+      }
+    }
+
   });
 
 });
