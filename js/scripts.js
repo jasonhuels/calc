5f1cd81385820f24be75602e5778e4c043b177a9
Jason Huels and Joel Stockamp <jason@>
2019-08-14 11:33:08 -0700

    add everything

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+
+});
+
+function add(number1, number2) {
+  return number1 + number2 ;
+}
+function sub(number1, number2) {
+  return number1 - number2 ;
+}
+function mult(number1, number2) {
+  return number1 * number2 ;
+}
+function divide(number1, number2) {
+  return number1 / number2 ;
+}
